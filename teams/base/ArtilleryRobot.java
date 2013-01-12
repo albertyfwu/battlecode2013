f8885a35d38b4897cd45be974a111edca99f1426
Tommy Liu <tommyyliu@gmail.com>
2013-01-12 01:15:40 -0500

    ArtilleryRobot now shoots

--- teams/base/ArtilleryRobot.java
+++ teams/base/ArtilleryRobot.java
@@ -1,18 +1,38 @@
 package base;
 
+import battlecode.common.MapLocation;
+import battlecode.common.Robot;
 import battlecode.common.RobotController;
 
 public class ArtilleryRobot extends BaseRobot {
 
 	public ArtilleryRobot(RobotController rc) {
 		super(rc);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		try{
+		if (rc.senseEncampmentSquares(rc.getLocation(), 63, rc.getTeam().opponent()).length==0){
+		}else{			
+		MapLocation placeToShoot= whereToShoot(rc.senseNearbyGameObjects(Robot.class, rc.getLocation(), 63, rc.getTeam().opponent()));
+		rc.attackSquare(placeToShoot);
+		}
+		}catch (Exception e) {
+			// Deal with exception
+		}
 	}
-
+	
+	public MapLocation whereToShoot(Robot[] potentialTargets){
+		int maxHits=0;
+		MapLocation bestLocation=rc.getLocation(potentialTargets[0]);
+		for (MapLocation location : potentialTargets){
+			int numNeighbors = rc.senseNearbyGameObjects(Robot.class,location,2,rc.getTeam().opponent()).length;
+			if (numNeighbors > maxHits){
+				maxHits = numNeighbors;
+				bestLocation=location;
+			}
+		}
+		return bestLocation;
+	}
 }
