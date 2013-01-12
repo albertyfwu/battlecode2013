e5b100a585d12f002c7217492be05f296fd42ddd
albertyfwu <albertyfwu@gmail.com>
2013-01-12 03:19:32 -0500

    fixed up some ArtilleryRobot code

--- teams/base/ArtilleryRobot.java
+++ teams/base/ArtilleryRobot.java
@@ -1,36 +1,41 @@
 package base;
 
+import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.Robot;
 import battlecode.common.RobotController;
 
 public class ArtilleryRobot extends BaseRobot {
-
+		
 	public ArtilleryRobot(RobotController rc) {
 		super(rc);
 	}
 
 	@Override
 	public void run() {
-		try{
-		if (rc.senseEncampmentSquares(rc.getLocation(), 63, rc.getTeam().opponent()).length==0){
-		}else{			
-		MapLocation placeToShoot= whereToShoot(rc.senseNearbyGameObjects(Robot.class, rc.getLocation(), 63, rc.getTeam().opponent()));
-		rc.attackSquare(placeToShoot);
-		}
-		}catch (Exception e) {
-			// Deal with exception
+		try {
+			if (rc.isActive()) {
+				Robot[] potentialTargets = rc.senseNearbyGameObjects(Robot.class, rc.getLocation(), 63, rc.getTeam().opponent());
+				MapLocation target = getBestTarget(potentialTargets);
+				if (target != null && rc.canAttackSquare(target)) {
+					rc.attackSquare(target);
+				}
+			}
+		} catch (Exception e) {
+			//
 		}
 	}
 	
-	public MapLocation whereToShoot(Robot[] potentialTargets){
-		int maxHits=0;
-		MapLocation bestLocation=rc.getLocation(potentialTargets[0]);
-		for (MapLocation location : potentialTargets){
-			int numNeighbors = rc.senseNearbyGameObjects(Robot.class,location,2,rc.getTeam().opponent()).length;
+	// TODO: Make sure GameActionException doesn't get thrown
+	public MapLocation getBestTarget(Robot[] potentialTargets) throws GameActionException {
+		int maxHits = 0;
+		MapLocation bestLocation = null;
+		for (Robot potentialTarget : potentialTargets){
+			MapLocation location = rc.senseLocationOf(potentialTarget);
+			int numNeighbors = rc.senseNearbyGameObjects(Robot.class, location, 2, rc.getTeam().opponent()).length;
 			if (numNeighbors > maxHits){
 				maxHits = numNeighbors;
-				bestLocation=location;
+				bestLocation = location;
 			}
 		}
 		return bestLocation;
--- teams/base/HQRobot.java
+++ teams/base/HQRobot.java
@@ -1,6 +1,5 @@
 package base;
 
-import battlecode.common.Clock;
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
