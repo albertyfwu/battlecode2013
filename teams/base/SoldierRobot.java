dfd88eff35b9184409ba8aa9f03f229a1706e616
albertyfwu <albertyfwu@gmail.com>
2013-01-14 01:46:23 -0500

    fixed off-by-one for backdoor navigation

--- teams/base/SoldierRobot.java
+++ teams/base/SoldierRobot.java
@@ -58,7 +58,7 @@ public class SoldierRobot extends BaseRobot {
 		try {
 			if (Clock.getRoundNum() < 2500) {
 				if (NavSystem.navMode == NavMode.NEUTRAL) {
-					NavSystem.setupSmartNav(new MapLocation(30, 27));
+					NavSystem.setupBackdoorNav(NavSystem.enemyHQLocation);
 				}
 				NavSystem.followWaypoints();
 //				if (soldierState != SoldierState.MINING_IN_CIRCLE) {
