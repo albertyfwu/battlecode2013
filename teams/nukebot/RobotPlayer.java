f505354dac92a2b48cad32f730e17937c1629b4b
albertyfwu <albertyfwu@gmail.com>
2013-01-15 02:38:51 -0500

    nuke bot

--- teams/nukebot/RobotPlayer.java
+++ teams/nukebot/RobotPlayer.java
@@ -1,7 +1,5 @@
 package nukebot;
 
-import java.util.Random;
-
 import battlecode.common.RobotController;
 import battlecode.common.RobotType;
 import battlecode.common.Upgrade;
@@ -10,13 +8,6 @@ public class RobotPlayer {
 	public static void run(RobotController rc) {
 		while (true) {
 			try {
-				Random randomGenerator = new Random();
-				for (int i = 1; i< 30; i++) {
-					int channel = randomGenerator.nextInt(10000);
-					int msg = randomGenerator.nextInt(10000);
-					rc.broadcast(channel, msg);
-				}
-
 				if (rc.getType()==RobotType.HQ){
 					if (!rc.hasUpgrade(Upgrade.NUKE)) {
 						rc.researchUpgrade(Upgrade.NUKE);
