2610998541e2efeeaebb9057e19d06ab7384e0b8
albertyfwu <albertyfwu@gmail.com>
2013-01-28 21:34:21 -0500

    fixed express charging for all_in

--- teams/alphaRetreat/EncampmentJobSystem.java
+++ teams/alphaRetreat/EncampmentJobSystem.java
@@ -304,7 +304,7 @@ public class EncampmentJobSystem {
 			
 		}
 		
-		rc.setIndicatorString(1, "not shield");
+//		rc.setIndicatorString(1, "not shield");
 		
 		for (int i = encampmentJobChannelList.length; --i >= 0; ) {
 			channel = encampmentJobChannelList[i];
